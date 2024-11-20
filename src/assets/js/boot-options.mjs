0f4ee7a10ab05e3ab7d4bb2e2c65360396f0e047
nicomiguelino <nicomiguelino2014@gmail.com>
2024-11-20 14:56:28 -0800

    chore(deps): migrate to Lit 3

--- src/assets/js/boot-options.mjs
+++ src/assets/js/boot-options.mjs
@@ -1,5 +1,21 @@
 'use strict';
 
-import {initOptions} from "./options.mjs";
+// import {initOptions} from "./options.mjs"; // TODO: Uncomment.
+import {html, LitElement} from 'lit';
 
-initOptions();
+// initOptions(); // TODO: Uncomment.
+
+export class OptionsPage extends LitElement {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return html`
+            <div class="container">
+                <h1>Hello, World!</h1>
+            </div>
+        `;
+    }
+}
+customElements.define('options-root', OptionsPage);
--- webpack.common.js
+++ webpack.common.js
@@ -13,6 +13,16 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
                 test: /\.s?css$/,
                 use: [
                     MiniCssExtractPlugin.loader,
