8942766b38fbf7581d54092907aacff3b238091b
nicomiguelino <nicomiguelino2014@gmail.com>
2024-12-10 15:19:46 -0800

    fix: make the error (about large cookies) more user-friendly

--- src/assets/js/components/popup/proposal.jsx
+++ src/assets/js/components/popup/proposal.jsx
@@ -189,7 +189,8 @@ export const Proposal = () => {
       headers = {
         'Cookie': currentProposal.cookieJar.map(
           cookie => cookiejs.serialize(cookie.name, cookie.value)
-        ).join('; ')
+        )
+        .join('; ')
       };
     }
 
@@ -262,6 +263,17 @@ export const Proposal = () => {
                 };
               });
             } else if (!errorJson.type) {
+              if (
+                errorJson.code == '23514' &&
+                errorJson.error == 'Constraint failed.'
+              ) {
+                throw `
+                  Screenly cannot save the web page's credentials because
+                  the cookies are too large. Please try again without
+                  saving the authentication.
+                `;
+              }
+
               throw JSON.stringify(errorJson);
             } else {
               throw 'Unknown error';
